f3105fc818e0258cec24b4a463e4ffee5d1bd0d6
Alexandru Toma <alexandru.toma15@imperial.ac.uk>
2017-11-01 11:42:31 +0000

    Initial commit

---
+++ SwiftChatLib/ChatViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftChatLib
+//
+//  Created by Alexandru Toma on 01/11/2017.
+//  Copyright © 2017 Alexandru Toma. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
