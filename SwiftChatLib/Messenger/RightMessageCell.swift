bb2389db18d1718acaef8331426c4dc824c89f92
Alexandru Toma <alexandru.toma15@imperial.ac.uk>
2017-11-08 15:34:51 +0000

    Chat app done

--- SwiftChatLib/Messenger/RightMessageCell.swift
+++ SwiftChatLib/Messenger/RightMessageCell.swift
@@ -11,7 +11,6 @@ import UIKit
 class RightMessengerCell: UICollectionViewCell {
     
     @IBOutlet weak var textView: DeletableTextView!
-    @IBOutlet weak var touchMask: RightCellTouchMask!
     fileprivate var delegate: MessengerBehavoirDelegate!
     fileprivate var isLast: Bool = false
     
@@ -22,7 +21,6 @@ class RightMessengerCell: UICollectionViewCell {
     func setDelegate(delegate: MessengerBehavoirDelegate) {
         self.delegate = delegate
         textView.setParent(parent: self)
-        touchMask.setParent(parent: self)
     }
     
     func setLast(isLast: Bool) {
@@ -30,38 +28,26 @@ class RightMessengerCell: UICollectionViewCell {
     }
 }
 
-class RightCellTouchMask: UIView {
-    
-    fileprivate var parent: RightMessengerCell!
-    
-    func setParent(parent: RightMessengerCell) {
-        self.parent = parent
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        parent.textView.showMenu()
-    }
-    
-}
-
 class DeletableTextView: UITextView {
     
     fileprivate var parent: RightMessengerCell!
+    fileprivate var menu: UIMenuController? = nil
     
     func setParent(parent: RightMessengerCell) {
         self.parent = parent
     }
     
     func showMenu() {
-        if parent.isLast {
-            let menu = UIMenuController()
-            menu.menuItems = [UIMenuItem(title: "Delete", action: #selector(deleteText))]
-            menu.update()
+        if parent.isLast && menu == nil {
+            menu = UIMenuController()
+            menu?.menuItems = [UIMenuItem(title: "Delete", action: #selector(deleteText))]
+            menu?.update()
         }
     }
     
     @objc func deleteText() {
         parent.delegate.deletedLastMessageFromMe()
+        menu = nil
     }
     
     override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
@@ -69,6 +55,10 @@ class DeletableTextView: UITextView {
             return true
         }
         return false
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        showMenu()
     }
     
 }
